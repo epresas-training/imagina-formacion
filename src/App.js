52d98e334b328f269569c00c5e6c1ee718a7c28f
Ed <edmundo.presas@kairosds.com>
2021-11-21 12:55:01 +0100

    :sparkles: Add events

--- src/App.js
+++ src/App.js
@@ -1,22 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef, createRef } from "react";
+
+import "./App.css";
 
 function App() {
+  const [userName, setUserName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [result, setResult] = useState("");
+  const counter = useRef(0);
+  const nameRef = createRef();
+  const lastNameRef = createRef();
+
+  useEffect(() => {
+    counter.current = counter.current + 1;
+  });
+
+  const handleClick = (event) => {
+    setResult(Math.random() + "");
+  };
+
+  const handleKeypress = (event) => {
+    if (event.charCode === 13) {
+      alert("Se ha pulsado la tecla ENTER");
+    }
+  };
+
+  const handleMouseIn = (event) => {
+    console.log("Entra el ratón", event);
+  };
+
+  const handleMouseOut = (event) => {
+    console.log("Sale el ratón", event);
+  };
+
+  const handleClickMouse = (event) => {
+    console.log(`El usuario pulsa en x: ${event.clientX}, y: ${event.clientY}`);
+  };
+
+  const handleTouch = (event) => {
+    console.log("El tipo de este evento es: ", event.type);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <h1>Formulario en React</h1>
+        <button onClick={handleClick}>Mostrar datos</button>
+        <strong>{result}</strong>
+        <div>
+          <input
+            placeholder="Nombre"
+            ref={nameRef}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+          <input
+            placeholder="Apellido"
+            ref={lastNameRef}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+        </div>
+        <div>El componente se ha renderizado: {counter.current}</div>
+        <hr />
+        <input placeholder="Se lanzará alerta" onKeyPress={handleKeypress} />
+        <input placeholder="No se lanzará alerta" />
+        <hr />
+        <button
+          onMouseEnter={handleMouseIn}
+          onMouseLeave={handleMouseOut}
+          onClick={handleClickMouse}
         >
-          Learn React
-        </a>
+          Pruebas con Eventos de ratón
+        </button>
+        <hr />
+        <button onTouchStart={handleTouch} onClick={handleTouch}>
+          Púlsame
+        </button>
       </header>
     </div>
   );
